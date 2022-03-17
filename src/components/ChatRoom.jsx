2a8cb77f8ec918885ad9d639e6c9429ffafec350
Mahendra <bus.mahendrat@gmail.com>
2022-03-18 00:34:22 +0100

    ongoing chat room, with minor css

---
+++ src/components/ChatRoom.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function ChatRoom() {
+
+  return (
+    <>
+      <Link to={`/chat/62327bd5474093b38f2c73bc`}>Chat</Link>
+    </>
+  )
+}
+
+export default ChatRoom;
