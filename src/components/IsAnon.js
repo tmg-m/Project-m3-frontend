e1a20f481dbdba310036e3d8ac35a49831823e48
Mahendra <bus.mahendrat@gmail.com>
2022-03-23 13:53:32 +0100

    last change, css and loading screen

--- src/components/IsAnon.js
+++ src/components/IsAnon.js
@@ -1,12 +1,13 @@
 import { useContext } from 'react';
 import { AuthContext } from '../context/auth.context';
 import { Navigate } from 'react-router-dom';
+import loadingImg from '../css/icons/loading.gif'
 
 function IsAnon({ children }) {
   const { isLoggedIn, isLoading } = useContext(AuthContext);
 
   // If the authentication is still loading ⏳
-  if (isLoading) return <p>Loading ...</p>;
+  if (isLoading) return <img className='loading-img' src={loadingImg}></img>;
 
   if (isLoggedIn) {
     // If the user is logged in, navigate to home page ❌
--- src/components/IsPrivate.js
+++ src/components/IsPrivate.js
@@ -1,12 +1,13 @@
 import { useContext } from 'react';
 import { AuthContext } from '../context/auth.context';
 import { Navigate } from 'react-router-dom';
+import loadingImg from '../css/icons/loading.gif'
 
 function IsPrivate({ children }) {
   const { isLoggedIn, isLoading } = useContext(AuthContext);
 
   // If the authentication is still loading ⏳
-  if (isLoading) return <div><p >OOOOOOOOOOO</p></div>;
+  if (isLoading) return <img loading-img src={loadingImg}></img>;
 
   if (!isLoggedIn) {
     // If the user is not logged in ❌
